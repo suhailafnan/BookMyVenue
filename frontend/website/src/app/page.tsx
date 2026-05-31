7be50b9d4dd89959947a05fedd4eefee56a7adb6
Nashva <nashvanasha123@gmail.com>
2026-05-31 23:30:03 +0530

    changed heading

--- frontend/website/src/app/page.tsx
+++ frontend/website/src/app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+        <div className="flex flgit ex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-blue-50">
+            WecodeMalayalam Suhail Munna Anandhu.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
