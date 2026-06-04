e74ee96181f3eb6dc2464f8704a7b17d6a6d49f0
munna-pp <maqsoodrahimanpp@gmail.com>
2026-06-04 13:18:35 +0530

    add search state handling

--- frontend/website/src/features/venues/components/SearchBar.tsx
+++ frontend/website/src/features/venues/components/SearchBar.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
 export default function SearchBar() {
+  const [search, setSearch] = useState("");
+
   return (
     <div>
       <input
         type="text"
         placeholder="Search venues..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
+
+      <p>You typed: {search}</p>
     </div>
   );
 }
