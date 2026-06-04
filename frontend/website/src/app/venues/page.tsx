83b589ba61adbcdb0aa4ab2b17a9df388c46daa0
munna-pp <maqsoodrahimanpp@gmail.com>
2026-06-04 14:14:49 +0530

    build venue listing page layout

--- frontend/website/src/app/venues/page.tsx
+++ frontend/website/src/app/venues/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
+import SearchBar from "@/features/venues/components/SearchBar";
 import VenueFilters from "@/features/venues/components/VenueFilters";
+import VenueGrid from "@/features/venues/components/VenueGrid";
 
 export default function VenuesPage() {
+  const [search, setSearch] = useState("");
+
   return (
     <main>
       <h1>Venue Listing Page</h1>
 
+      <SearchBar
+        search={search}
+        setSearch={setSearch}
+      />
+
       <VenueFilters />
+
+      <VenueGrid search={search} />
     </main>
   );
 }
