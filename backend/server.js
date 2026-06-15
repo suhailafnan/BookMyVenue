7aaed2a64fb72e6793b5200c184a9067716a5327
munna-pp <maqsoodrahimanpp@gmail.com>
2026-06-15 16:15:38 +0530

    setup backend and connect mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -7,10 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Products API Running");
+  res.send("BookMyVenue Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
